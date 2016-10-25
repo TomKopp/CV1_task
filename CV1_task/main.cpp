a26d283ed18ca8add5201a1b61369a8250bd17d8
Tom Kopp <witzbould@live.com>
2016-10-25 22:18:49 +0200

    add comments

--- CV1_task/main.cpp
+++ CV1_task/main.cpp
@@ -50,6 +50,7 @@ static Mat convertImgToBGR(Mat& ImgOrig) {
 static int getHueDistance(Mat& Img, Point A, Point B) {
   uchar A_val, B_val, deltaHue;
 
+  // ckeck if Points are within Mat, else the hue distance is huge
   if (
     (A.x < 0
     || A.y < 0
@@ -64,10 +65,12 @@ static int getHueDistance(Mat& Img, Point A, Point B) {
     return INT_MAX;
   }
 
+  // calculate hue distance
   A_val = Img.at<uchar>(A);
   B_val = Img.at<uchar>(B);
   deltaHue = A_val - B_val;
 
+  // distances are always positive; mathematically correct would be sqrt(pow((double)(A_val - B_val), 2)) - but overkill
   if (deltaHue < 0) {
     deltaHue *= (-1);
   }
@@ -75,7 +78,7 @@ static int getHueDistance(Mat& Img, Point A, Point B) {
   return (int)deltaHue;
 }
 
-static Point getNextPix(Mat& Img, Point P, Point Last) {
+static Point getNextPix(Mat& Img, Vertices& List, Point P) {
   // all adjacent points with initial hue distance
   //PointsHueDistance adjacentPix = {
   //  { Point(p.x - 1, p.y - 1), INT_MAX },{ Point(p.x, p.y - 1), INT_MAX },{ Point(p.x + 1, p.y - 1), INT_MAX },
@@ -167,7 +170,8 @@ static Point getNextPix(Mat& Img, Point P, Point Last) {
   while (_begin != _end) {
     PointHueDistance& item = *_begin;
     ++_begin;
-    if (item.p == Last) continue;
+    // if Point was already choosen before, don't update the hue distance
+    if ( find(List.begin(), List.end(), item.p) != List.end() ) continue;
     item.dHue = getHueDistance(Img, P, item.p);
   }
 
@@ -177,6 +181,7 @@ static Point getNextPix(Mat& Img, Point P, Point Last) {
 }
 
 static void drawPathBGR(Mat Img, Vertices Path, uchar blue = 0, uchar green = 0, uchar red = 0) {
+  // draw every Point from Path, colors need to be given else it's black
   auto _begin = Path.begin(), _end = Path.end();
   while (_begin != _end) {
     Point& item = *_begin;
@@ -186,7 +191,7 @@ static void drawPathBGR(Mat Img, Vertices Path, uchar blue = 0, uchar green = 0,
 }
 
 static void onMouse(int event, int x, int y, int, void* PointsList) {
-  // Proceed only if left mouse button was pressed
+  // proceed only if left mouse button was pressed
   if (event != EVENT_LBUTTONDOWN) {
     return;
   }
@@ -207,10 +212,10 @@ static void onMouse(int event, int x, int y, int, void* PointsList) {
 
   // runs the path discovery - builds a list of Point's that define the path
   while (List->back() != EndPoint) {
-    List->push_back(getNextPix(ImgGray, List->back(), *(List->end() -2) ));
+    List->push_back( getNextPix(ImgGray, *List, List->back()) );
   }
 
-  // Print the path in the image
+  // print the path in the image
   drawPathBGR(ImgRes, *List, 0, 255);
   imshow("Result", ImgRes);
 }
@@ -220,35 +225,33 @@ static void onMouse(int event, int x, int y, int, void* PointsList) {
 int main(int argc, char** argv) {
   Vertices PointsList = {};
 
-  // Check if image path is supplied as argument
+  // check if image path is supplied as argument
   if (argc < 2) {
     cout << "Path must be applied as commandline argument." << endl;
     return -1;
   }
 
-  // Read image and check if successful
+  // read image and check if successful
   ImgOrig = imread(argv[1]);
   if (ImgOrig.empty()) {
     cout << "Could not open or find the image." << endl;
     return -1;
   }
 
-  // Convert given image to gray edge image
-  //Mat ImgEdge = convertImgToEdge(ImgOrig);
+  // convert given image to gray edge image
   ImgGray = convertImgToGray(ImgOrig);
   ImgRes = convertImgToBGR(ImgGray);
 
-  // Create a window for display
+  // create a window for display
   namedWindow("Output");
   namedWindow("Result");
-  // Listen to mouse events
+  // listen to mouse events
   setMouseCallback("Output", onMouse, &PointsList);
-  // Display imge in window
-  imshow("Output", ImgGray);
-  //imshow("Output", ImgOrig);
+  // display imge in window
+  imshow("Output", ImgOrig);
   
 
-  // Wait for a keystroke in the window
+  // wait for a keystroke in the window
   waitKey();
   return 0;
 }
