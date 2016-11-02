265003c831d11c1f1a3acb163f265dd06eb0ba4c
witzbould <witzbould@WITZBOULDDELL>
2016-11-02 17:25:38 +0100

    add main stub

--- CV1_task2/main.cpp
+++ CV1_task2/main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+
 int main(int argc, char** argv) {
+  cv::Mat ImgOrig;
+  // check if image path is supplied as argument
+  if (argc < 2) {
+    std::cout << "Path must be applied as commandline argument." << std::endl;
+    return -1;
+  }
+
+  // read image and check if successful
+  ImgOrig = cv::imread(argv[1]);
+  if (ImgOrig.empty()) {
+    std::cout << "Could not open or find the image." << std::endl;
+    return -1;
+  }
+
+
+  // create a window for display
+  cv::namedWindow("Original");
+  cv::namedWindow("Result");
+
+  cv::waitKey();
   return 0;
 }
