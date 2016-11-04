7dcdd42d1cc573c0fbad946b06f1a7b2a9d1bdef
Tom Kopp <witzbould@live.com>
2016-11-04 01:55:47 +0100

    try to implement sobel filter - failed at walking through cv::Mat propperly

--- CV1_task2/main.cpp
+++ CV1_task2/main.cpp
@@ -2,8 +2,103 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "Utils.h"
+
+
+
+static cv::Mat applySobelX(const cv::Mat& Img) {
+  // Accept only char type matrices
+  CV_Assert(Img.depth() == CV_8U);
+
+  cv::Mat Res;
+  const int nChannels = Img.channels();
+  int scan_start_x = 0
+    , scan_start_y = 0
+    , scan_end_x = (Img.cols - 1)
+    , scan_end_y = (Img.rows - 1);
+  uint val_nw, val_ne, val_e, val_se, val_sw, val_w, r,l,m;
+  uchar *my_point
+    , *ptr_row
+    , *ptr_row_prev
+    , *ptr_row_next;
+
+  Res.create(Img.size(), Img.type());
+
+  //if (Img.isContinuous()) {
+  //  ptr_row_prev = Img.data;
+  //  ptr_row = ptr_row_prev * Img.cols * Img.channels();
+  //}
+
+
+
+  //while (scan_start_y < scan_end_y)
+  //{
+  //  const uchar *ptr_row_prev = Img.ptr<uchar>(scan_start_y);
+  //  const uchar *ptr_row = Img.ptr<uchar>(++scan_start_y);
+  //  const uchar *ptr_row_next = Img.ptr<uchar>(scan_start_y + 1);
+
+  //  uchar *ptr_out = Res.ptr<uchar>(scan_start_y);
+
+  //  while (scan_start_x < scan_end_x)
+  //  {
+  //    r = scan_start_x;
+  //    m = ++scan_start_x;
+  //    l = m + 1;
+  //    val_nw = ptr_row_prev[r];
+  //    val_w = ptr_row[r];
+  //    val_sw = ptr_row_next[r];
+
+  //    //my_point = &ptr_row[m];
+
+  //    val_ne = ptr_row_prev[l];
+  //    val_e = ptr_row[l];
+  //    val_se = ptr_row_next[l];
+
+  //    // Confused yet? Fear not, it will get worse.
+  //    //*my_point = val_ne + (val_e << 1) + val_se - val_nw - (val_w << 1) - val_sw;
+  //    ptr_out[m] = 222;
+  //  }
+  //}
+
+  while (scan_start_y < scan_end_y)
+  {
+    ptr_row = Res.ptr<uchar>(++scan_start_y);
+
+    while (scan_start_x < scan_end_x)
+    {
+      my_point = &ptr_row[ (++scan_start_x * nChannels)];
+      *my_point = 222;
+    }
+  }
+
+  //for (scan_start_y; scan_start_y < scan_end_y; scan_start_y++)
+  //{
+  //  uchar *ptr_out = Res.ptr<uchar>(scan_start_y);
+
+  //  for (scan_start_x; scan_start_x < scan_end_x; scan_start_x++)
+  //  {
+  //    my_point = &ptr_out[scan_start_x];
+  //    *my_point = 222;
+  //  }
+  //}
+
+  return Res;
+}
+
+static cv::Mat applySobel(const cv::Mat& Img) {
+  cv::Mat Res;
+  Res.create(Img.size(), Img.type());
+
+  cv::Mat kern = (cv::Mat_<char>(3, 3) << -1, 0, 1,   -2, 0, 2,   -1, 0, 1);
+
+  cv::filter2D(Img, Res, Img.depth(), kern);
+
+  return Res;
+}
+
+
 
 int main(int argc, char** argv) {
   cv::Mat ImgOrig, ImgResult;
@@ -29,7 +124,7 @@ int main(int argc, char** argv) {
 
   // Display Images
   cv::imshow("Original", ImgOrig);
-  cv::imshow("Result", ImgResult);
+  cv::imshow("Result", applySobelX(ImgResult));
 
   cv::waitKey();
   return 0;
