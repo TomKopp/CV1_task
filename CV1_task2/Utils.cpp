d5371ed8df15c523aee6f2ba85b5814c492a2a32
witzbould <witzbould@WITZBOULDDELL>
2016-11-07 19:09:28 +0100

    rename variables, remove iostream include, reformate code (kinda)

--- CV1_task2/Utils.cpp
+++ CV1_task2/Utils.cpp
@@ -1,7 +1,5 @@
 #include "Utils.h"
 
-#include <iostream>
-
 #include <list>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -41,7 +39,7 @@ bool Utils::isPointWithinMat(const cv::Mat & Img, const cv::Point & Pnt)
   return !isPointOffMat(Img, Pnt);
 }
 
-void Utils::drawPointInMat(cv::Mat & Img, const cv::Point & Pnt, const cv::Vec3b & Color = cv::Vec3b(0,0,0))
+void Utils::drawPointInMat(cv::Mat & Img, const cv::Point & Pnt, const cv::Vec3b & Color = cv::Vec3b(0, 0, 0))
 {
   Img.at<cv::Vec3b>(Pnt) = Color;
 }
@@ -57,51 +55,45 @@ cv::Mat Utils::convolveMatWithSobel(const cv::Mat & Img)
   CV_Assert(Img.depth() == CV_8U);
 
   cv::Mat Res;
-  int nRows = Img.rows - 1;
-  int nCols = (Img.cols - 1) * Img.channels();
+  int row_count = Img.rows - 1;
+  int col_count = (Img.cols - 1) * Img.channels();
   int i,
     j,
     val_col_left,
     val_col_right,
-    result;
-  const uchar *row,
+    val_result;
+  const uchar *row_cur,
     *row_prev,
     *row_next;
   uchar *row_res;
-  std::list<int> blubber;
-
-  //if (Img.isContinuous()) {
-  //  nCols *= nRows;
-  //  nRows = 1;
-  //}
+  std::list<int> cols_calculated;
 
   Res.create(Img.size(), Img.type());
   Res = cv::Scalar::all(0);
 
-  for (i = 1; i < nRows; ++i) {
-    row_prev = Img.ptr<uchar>(i-1);
-    row = Img.ptr<uchar>(i);
+  for (i = 1; i < row_count; ++i) {
+    row_prev = Img.ptr<uchar>(i - 1);
+    row_cur = Img.ptr<uchar>(i);
     row_res = Res.ptr<uchar>(i);
-    row_next = Img.ptr<uchar>(i+1);
-    blubber.clear();
+    row_next = Img.ptr<uchar>(i + 1);
+    cols_calculated.clear();
 
-    for (j = 1; j < nCols; ++j) {
-      if (blubber.size() < 2) {
-        val_col_left = row_prev[j - 1] + ((uint)row[j - 1] << 1) + row_next[j - 1];
+    for (j = 1; j < col_count; ++j) {
+      if (cols_calculated.size() < 2) {
+        val_col_left = row_prev[j - 1] + ((uint)row_cur[j - 1] << 1) + row_next[j - 1];
       }
       else {
-        val_col_left = blubber.front();
-        blubber.pop_front();
+        val_col_left = cols_calculated.front();
+        cols_calculated.pop_front();
       }
 
-      val_col_right = row_prev[j + 1] + ((uint)row[j + 1] << 1) + row_next[j + 1];
-      result = val_col_right - val_col_left;
-      
-      blubber.push_back(val_col_right);
-      row_res[j] = cv::saturate_cast<uchar>(result);
+      val_col_right = row_prev[j + 1] + ((uint)row_cur[j + 1] << 1) + row_next[j + 1];
+      val_result = val_col_right - val_col_left;
+
+      cols_calculated.push_back(val_col_right);
+      row_res[j] = cv::saturate_cast<uchar>(val_result);
     }
   }
 
   return Res;
 }
-
